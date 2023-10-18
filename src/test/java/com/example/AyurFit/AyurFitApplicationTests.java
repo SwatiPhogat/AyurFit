58b89a75458dd63f1381ee8139f157de42109de6
Swati <@gmail.com>
2023-10-18 11:10:54 +0000

    Init the git

---
+++ src/test/java/com/example/AyurFit/AyurFitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.AyurFit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AyurFitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
